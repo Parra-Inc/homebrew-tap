6467087ee3ae4aa5893d027dee79fc901246fbc1
Mick MacCallum <micksmaccallum@gmail.com>
2024-07-24 18:14:53 -0400

    preparing for webhook job

--- Formula/parra-cli.rb
+++ Formula/parra-cli.rb
@@ -1,9 +1,10 @@
 VERSION = "0.1.19"
 ARM_SHA = "8b11a53312c44a3a8d9d94718643f709b0d27ff0d4fd984365416203c44d811c"
 X86_SHA = "5ac46490ed1df953dae4202867f449fefad28af74fd3a91b548f57a8ea42fc3a"
+DESCRIPTION = "A utility for getting started with your next Parra.io project."
 
 class ParraCli < Formula
-  desc "A utility for getting started with your next Parra.io project."
+  desc DESCRIPTION
   homepage "https://parra.io"
   license "MIT"
   url "https://github.com/Parra-Inc/parra-cli.git", tag: VERSION
@@ -29,3 +30,4 @@ class ParraCli < Formula
     end    
   end
 end
+
