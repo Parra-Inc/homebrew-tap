de08608ef81400f229b27ace7021d1dfbb507d68
Mick MacCallum <micksmaccallum@gmail.com>
2024-07-30 17:01:26 +0000

    Parra CLI version

--- Formula/parra-cli.rb
+++ Formula/parra-cli.rb
@@ -1,6 +1,6 @@
-VERSION = "0.1.44"
-ARM_SHA = "e72cc307963ffca7b8a0f30ff9e2119ec20d190ccd8a6ead80d0bab1e36cc48b"
-X86_SHA = "b590ea9fd86e9fe08a60de39042155ba349380bdb732a866c99631f51cee44c3"
+VERSION = "0.1.1"
+ARM_SHA = "stuff"
+X86_SHA = "stuff"
 DESCRIPTION = "A utility for getting started with your next Parra.io project."
 
 class ParraCli < Formula
@@ -8,6 +8,7 @@ class ParraCli < Formula
   homepage "https://parra.io"
   license "MIT"
   url "https://github.com/Parra-Inc/parra-mobile-sdks.git", tag: VERSION
+
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Parra-Inc/parra-mobile-sdks/releases/download/v#{VERSION}/parra-cli-#{VERSION}.aarch64_apple_darwin.tar.gz"
