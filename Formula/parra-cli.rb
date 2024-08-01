c593f7250d4ba9b362908be8441d6b1c0cdbb5b5
Mick MacCallum <micksmaccallum@gmail.com>
2024-08-01 16:13:52 +0000

    Parra CLI version "0.1.58"

--- Formula/parra-cli.rb
+++ Formula/parra-cli.rb
@@ -1,6 +1,6 @@
-VERSION = "0.1.57"
-ARM_SHA = "face5e49dd2bca9b42bed92f17400fe12c9677e29539bba378d608be7d007a8a"
-X86_SHA = "3e86fb2f85b08f08386ef40904b5be701250d7996cfb6fc88d46d389453e3b13"
+VERSION = "0.1.58"
+ARM_SHA = "d345b5b1fdbe58aee4b911f11d103c01510863de26e56d9aa59b09fd52009d1d"
+X86_SHA = "b68dd2e642f8b9286d400f33124f171330f7a56b878b4b6056d3c157b1b2ca1b"
 DESCRIPTION = "A utility for getting started with your next Parra.io project."
 
 class ParraCli < Formula
@@ -11,10 +11,10 @@ class ParraCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Parra-Inc/parra-mobile-sdks/releases/download/v#{VERSION}/parra-cli-#{VERSION}.aarch64_apple_darwin.tar.gz"
+      url "https://github.com/Parra-Inc/parra-mobile-sdks/releases/download/#{VERSION}/parra-cli-#{VERSION}.aarch64_apple_darwin.tar.gz"
       sha256 ARM_SHA
     elsif Hardware::CPU.intel?
-      url "https://github.com/Parra-Inc/parra-mobile-sdks/releases/download/v#{VERSION}/parra-cli-#{VERSION}.x86_64_apple_darwin.tar.gz"
+      url "https://github.com/Parra-Inc/parra-mobile-sdks/releases/download/#{VERSION}/parra-cli-#{VERSION}.x86_64_apple_darwin.tar.gz"
       sha256 X86_SHA
     end
   end
