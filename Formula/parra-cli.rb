f76f32e866e93ba038d129ed4b589f3dd9ddcf24
Mick MacCallum <micksmaccallum@gmail.com>
2024-09-09 12:39:35 +0000

    Parra CLI version "0.3.3"

--- Formula/parra-cli.rb
+++ Formula/parra-cli.rb
@@ -1,6 +1,6 @@
-VERSION = "0.3.2"
-ARM_SHA = "ca01538c3a29b8012e9dfa6f64f22eddb54c924741dd71b84a85970bb85dfea6"
-X86_SHA = "c8f10952d70bc4164f060695ae6ad700e19abb83fe1dd495f896007d985d11f8"
+VERSION = "0.3.3"
+ARM_SHA = "e1778248efe4283e92f851dea5b9e74e26bbe6e471f9c523e5e1755484773980"
+X86_SHA = "9f253174e07a36eaf6a6e02c5c9cf701c903660576865e18fc82b2f5af38c559"
 DESCRIPTION = "A utility for getting started with your next Parra.io project."
 
 class ParraCli < Formula
